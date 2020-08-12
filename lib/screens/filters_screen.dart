a8d6b7f79929b6d2862176f5e75c1bce38c71d98
Chimezirim Ekemam <ekemammezez@gmail.com>
2020-08-12 11:34:23 +0100

    filters scaffold update

--- lib/screens/filters_screen.dart
+++ lib/screens/filters_screen.dart
@@ -14,6 +14,15 @@ class _FiltersScreenState extends State<FiltersScreen> {
   bool _vegan = false;
   bool _lactoseFree = false;
 
+  Widget _buildSwitchListTile(String title, String description,
+      bool currentValue, Function updateValue) {
+    return SwitchListTile(
+        title: Text(title),
+        value: currentValue,
+        subtitle: Text(description),
+        onChanged: updateValue);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,15 +43,32 @@ class _FiltersScreenState extends State<FiltersScreen> {
           Expanded(
               child: ListView(
             children: <Widget>[
-              SwitchListTile(
-                  title: Text('Gluten-free'),
-                  value: _glutenFree,
-                  subtitle: Text('Only include gluten free meals'),
-                  onChanged: (newValue) {
-                    setState(() {
-                      _glutenFree = newValue;
-                    });
-                  })
+              _buildSwitchListTile(
+                  'Gluten-free', 'Only include gluten-free meals', _glutenFree,
+                  (newValue) {
+                setState(() {
+                  _glutenFree = newValue;
+                });
+              }),
+              _buildSwitchListTile('Lactose-free',
+                  'Only include lactose-free meals', _lactoseFree, (newValue) {
+                setState(() {
+                  _lactoseFree = newValue;
+                });
+              }),
+              _buildSwitchListTile(
+                  'Vegetarian', 'Only include vegetarian meals', _vegetarian,
+                  (newValue) {
+                setState(() {
+                  _vegetarian = newValue;
+                });
+              }),
+              _buildSwitchListTile('Vegan', 'Only include vegan meals', _vegan,
+                  (newValue) {
+                setState(() {
+                  _vegan = newValue;
+                });
+              }),
             ],
           ))
         ],
